22bbcb4e28336d6988e2ad425ab4405d9d089c9f
Sonic19972009 <sonic19972009@yandex.ru>
2025-08-25 00:22:30 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const orangeBlocks = document.getElementsByClassName('reveal');
+
+window.addEventListener('scroll', function() {
+  for (const block of orangeBlocks) {
+    const rect = block.getBoundingClientRect();
+    block.classList.toggle('reveal_active', (rect.top < window.innerHeight) && (rect.bottom > 0));
+  }  
+})
+  
