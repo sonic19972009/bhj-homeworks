9837e41997520861e5b431dd49cf13519f3bc330
Sonic19972009 <sonic19972009@yandex.ru>
2025-08-26 00:37:48 +0300

    changes to the mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,24 +4,25 @@ const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 const getHole = index => document.getElementById(`hole${index}`);
 
-const clear = () => {
+const clear = (message) => {
+  alert(message);
   dead.textContent = 0;
   lost.textContent = 0;
 }
 
 for (let i = 1; i < 10; i++) {
   getHole(i).onclick = () => {
-    if (getHole(i).className.includes('hole_has-mole'))
+    if (getHole(i).className.includes('hole_has-mole')) {
       dead.textContent++;
-    else
+    } else {
       lost.textContent++;
+    }
+
     if (dead.textContent == 10) {
-      alert('Победа!');
-      clear();
+      clear('Победа!');
     }
     if (lost.textContent == 5) {
-      alert('Вы проиграли');
-      clear();
+      clear('Вы проиграли!');
     }
   }
 }
